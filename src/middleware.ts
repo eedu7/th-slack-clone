5bae1577f398dc130fe920f813cc5c907701a763
eedu7 <mueedahmad067@gmail.com>
2024-09-20 09:39:23 +0500

    MINOR: added the middleware and some authenticated route settings

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { nextjsMiddlewareRedirect, convexAuthNextjsMiddleware, createRouteMatcher, isAuthenticatedNextjs } from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/auth"])
+
+export default convexAuthNextjsMiddleware((request) => {
+    if (!isPublicPage(request) && !isAuthenticatedNextjs()) {
+        return nextjsMiddlewareRedirect(request, "/auth")
+    }
+    // TODO: Redirect user away from "/auth" if authenticated
+});
+
+export const config = {
+    // The following matcher runs middleware on all routes
+    // except static assets.
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
