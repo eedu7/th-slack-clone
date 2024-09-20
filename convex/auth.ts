96f5a058ea7bddef15e6d685934fa1c4a762e4cd
eedu7 <mueedahmad067@gmail.com>
2024-09-20 11:29:00 +0500

    FEAT: completed the password authentication also implemented the sign up logic

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,8 +1,9 @@
 
 import { convexAuth } from "@convex-dev/auth/server";
 import GitHub from "@auth/core/providers/github";
+import {Password} from "@convex-dev/auth/providers/Password";
 import Google from "@auth/core/providers/google";
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [GitHub, Google],
+  providers: [GitHub, Google, Password],
 });
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -6,8 +6,9 @@ import {Separator} from "@/components/ui/separator";
 import {FcGoogle} from "react-icons/fc";
 import {FaGithub} from "react-icons/fa";
 import {SignInFlow} from "@/features/auth/types";
-import {useState} from "react";
+import React, {useState} from "react";
 import { useAuthActions } from "@convex-dev/auth/react";
+import {TriangleAlert} from "lucide-react";
 
 interface SignInCardProps {
     setState: (state: SignInFlow) => void;
@@ -21,6 +22,19 @@ export const SignInCard = ({setState}: SignInCardProps) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [pending, setPending] = useState(false);
+    const [error, setError] = useState("");
+
+    const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setPending(true);
+
+        signIn("password", {email, password, flow: "signIn"})
+            .catch(() => {
+                setError("Invalid email or password");
+            })
+            .finally(() => {setPending(false);});
+
+    }
 
     const onProviderSignIn = (value: "github" | "google") => {
         setPending(true);
@@ -40,8 +54,14 @@ export const SignInCard = ({setState}: SignInCardProps) => {
                 Use your email or another service to continue
             </CardDescription>
             </CardHeader>
+            {!!error && (
+                <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+                    <TriangleAlert className="size-4" />
+                    <p>{error}</p>
+                </div>
+            )}
             <CardContent className="space-y-5 px-0 pb-0">
-                <form className="space-y-2.5">
+                <form onSubmit={onPasswordSignIn} className="space-y-2.5">
                     <Input
                     disabled={pending}
                     value={email}
